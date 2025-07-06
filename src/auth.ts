0694f152d07a6db299d92256a95b00d249576a11
Sagar-v4 <sagarvariya4@gmail.com>
2025-07-07 01:36:10 +0530

    fix(auth): add trust host as true
    Without trustHost as true NextAuth obj, it does not running after successful build

--- src/auth.ts
+++ src/auth.ts
@@ -3,6 +3,7 @@ import Google from 'next-auth/providers/google';
 import GitHub from 'next-auth/providers/github';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  trustHost: true,
   secret: String(process.env.AUTH_JWT_SECRET),
   jwt: {
     maxAge: Number(process.env.AUTH_JWT_TOKEN_EXPIRE_IN_SEC),
