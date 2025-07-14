9ce0e183b64085327db99dfe4b36085e4b65f5ca
Sagar-v4 <sagarvariya4@gmail.com>
2025-07-14 20:13:19 +0530

    refactor(logout): refactor logout ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import { auth } from '@/auth';
 import { RoomForm } from '@/components/forms/room';
 import { LoginForm } from '@/components/forms/login';
@@ -8,6 +9,9 @@ export default async function Home() {
   return (
     <>
       <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+        <div className="absolute top-0 left-2">
+          <Image src="/logo.png" alt="Room Logo" width={70} height={70} />
+        </div>
         <div className="flex w-full max-w-sm flex-col gap-6">
           {session?.user ? <RoomForm /> : <LoginForm />}
         </div>
--- src/components/forms/user-info.tsx
+++ src/components/forms/user-info.tsx
@@ -1,33 +1,64 @@
 import { LogOut } from 'lucide-react';
 import { auth, signOut } from '@/auth';
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
 import { Button } from '@/components/ui/button';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 
 export async function UserForm() {
   const session = await auth();
   return (
     <>
-      <div className="absolute top-2 right-2 ml-2 bg-card flex  rounded-lg border p-4 shadow-sm gap-2">
-        <Avatar className="size-9 rounded-lg">
-          <AvatarImage src={session?.user?.image as string} />
-          <AvatarFallback>{session?.user?.name}</AvatarFallback>
-        </Avatar>
-        <div className="grid flex-1 text-left text-sm leading-tight">
-          <span className="truncate font-medium">{session?.user?.name}</span>
-          <span className="text-muted-foreground truncate text-xs">
-            {session?.user?.email}
-          </span>
-        </div>
-        <form
-          action={async () => {
-            'use server';
-            await signOut();
-          }}
-        >
-          <Button type="submit" variant="destructive" title="Logout">
-            <LogOut />
-          </Button>
-        </form>
+      <div className="absolute top-3 right-4 ml-2 flex gap-2">
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-auto p-0 rounded-full border">
+              <Avatar className="size-11.5 rounded-full">
+                <AvatarImage src={session?.user?.image as string} />
+                <AvatarFallback>{session?.user?.name}</AvatarFallback>
+              </Avatar>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="mr-4">
+            <DropdownMenuLabel className="flex min-w-0 flex-col">
+              <span className="text-foreground truncate text-sm font-medium">
+                {session?.user?.name}
+              </span>
+              <span className="text-muted-foreground truncate text-xs font-normal">
+                {session?.user?.email}
+              </span>
+            </DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem asChild className="p-0">
+              <form
+                action={async () => {
+                  'use server';
+                  await signOut();
+                }}
+              >
+                <Button
+                  type="submit"
+                  variant="ghost"
+                  title="Logout"
+                  className="size-full justify-start"
+                >
+                  <LogOut
+                    size={16}
+                    className="text-destructive"
+                    aria-hidden="true"
+                  />
+                  <span className="text-destructive">Logout</span>
+                </Button>
+              </form>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </>
   );
